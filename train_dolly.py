af91832c663025b9d5b3bd0e01be125b91a51799
Matthew Hayes <matt.hayes@databricks.com>
2023-03-27 22:44:13 -0700

    Add response generation for examples from blog post (#27)
    This adds response generation code and runs it over the examples from the [blog post](https://www.databricks.com/blog/2023/03/24/hello-dolly-democratizing-magic-chatgpt-open-models.html).
    
    A few of the sample outputs:
    
    **Instruction:** Write a love letter to Edgar Allan Poe.
    
    > My love for you is as eternal and enduring as the starlit nights, as deep as the ocean depths, as strong as the wind, and as sweet as the summer breeze. My heart has been yours since the first day I saw you, and it will remain yours forever. I can see you now and my heart fills with longing for our embrace, as I long to be with you forever.
    
    **Instruction:** Explain to me the difference between nuclear fission and fusion.
    
    > Nuclear fission is the splitting of a nucleus, which is a heavy atom, such as uranium, into two separate nuclei, while nuclear fusion is the joining of two nuclei to form a single nucleus, such as hydrogen and helium. Fission occurs when the nucleus absorbs a neutron, leaving a remnant of the nucleus. Fusion occurs when the two nuclei come together and form a single nucleus. Fission is a nuclear reaction and fusion is a nuclear reaction.
    
    **Instruction:** Write a tweet announcing Dolly, a large language model from Databricks.
    
    > Dolly, the world's most accurate language model, is now available on Databricks! 🎉 🎉 🎉
    
    **Instruction:** I'm selling my Nikon D-750, write a short blurb for my ad.
    
    > I'm selling my Nikon D-750, an amazing camera. It has the latest features including 12MP CMOS sensor, 9-point autofocus and auto bracketing, and 3.0x optical zoom. It also has a cool Image Stabilizer system and a dual SD card slot. It is a great choice for photographers looking for a reliable camera that offers superior image quality and features.
    
    **Instruction:** Give me a list of 5 science fiction books I should read next.
    
    > 1. The Hitchhiker's Guide to the Galaxy by Douglas Adams 
    > 2. Ender’s Game by Orson Scott Card 
    > 3. 1984 by George Orwell 
    > 4. Star Wars by George Lucas 
    > 5. The Hitchhiker's Guide to the Galaxy by Douglas Adams

--- train_dolly.py
+++ train_dolly.py
@@ -114,3 +114,24 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 # MAGIC     --lr 1e-5
 
 # COMMAND ----------
+
+from training.generate import generate_response, load_model_tokenizer_for_generate
+
+model, tokenizer = load_model_tokenizer_for_generate(local_output_dir)
+
+# COMMAND ----------
+
+# Examples from https://www.databricks.com/blog/2023/03/24/hello-dolly-democratizing-magic-chatgpt-open-models.html
+instructions = [
+    "Write a love letter to Edgar Allan Poe.",
+    "Write a tweet announcing Dolly, a large language model from Databricks.",
+    "I'm selling my Nikon D-750, write a short blurb for my ad.",
+    "Explain to me the difference between nuclear fission and fusion.",
+    "Give me a list of 5 science fiction books I should read next.",
+]
+
+# Use the model to generate responses for each of the instructions above.
+for instruction in instructions:
+    response = generate_response(instruction, model=model, tokenizer=tokenizer)
+    if response:
+        print(f"Instruction: {instruction}\n\n{response}\n\n-----------\n")
