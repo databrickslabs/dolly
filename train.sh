c89f48d2648f05c742770e9d856af3adc50ebd14
lbwavebo2022 <bo.ling@uber.com>
2023-04-04 23:20:56 +0000

    bug fixing

--- train.sh
+++ train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 deepspeed \
-     --include="localhost:3,2" \
+     --include="localhost:1,0" \
      --module training.trainer \
      --deepspeed config/ds_z3_bf16_config.json \
      --epochs 1 \
-     --local-output-dir /home/bo_ling/dolly/dolly_training \
+     --local-output-dir /home/bo_ling/dolly_training \
      --per-device-train-batch-size 2 \
      --per-device-eval-batch-size 2 \
      --lr 1e-5
