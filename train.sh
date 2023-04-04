f98d2a7e7264d38bc990e615194248e2e200c181
lbwavebo2022 <bo.ling@uber.com>
2023-04-04 18:20:06 +0000

    change config

--- train.sh
+++ train.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-deepspeed --num_gpus=2 \
+deepspeed \
+     --include="localhost:3,2" \
      --module training.trainer \
      --deepspeed config/ds_z3_bf16_config.json \
      --epochs 1 \
-     --local-output-dir dolly_training \
-     --per-device-train-batch-size 8 \
-     --per-device-eval-batch-size 8 \
+     --local-output-dir /home/bo_ling/dolly/dolly_training \
+     --per-device-train-batch-size 2 \
+     --per-device-eval-batch-size 2 \
      --lr 1e-5
